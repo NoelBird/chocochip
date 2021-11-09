a574d1f47b17cea7812a3acd831d029941ac080b
noelbird <lduldu00228@gmail.com>
2021-11-10 00:09:17 +0900

    update 155_min_stack

--- sites/leetcode/155_min_stack/1.cpp
+++ sites/leetcode/155_min_stack/1.cpp
@@ -1,33 +1,51 @@
+// inefficient processing exists. becuase I use vector space x2
+
+#include<vector>
+
+using namespace std;
+
 class MinStack {
 public:
-    int _min;
     vector<int> _stack;
-    int top;
+    vector<int> _val_stack;
+    int _top;
         
     MinStack() {
         vector<int> tmp;
         _stack.swap(tmp);
-        top = 0;
+        _top = 0;
     }
     
     void push(int val) {
-        if(val < _min)
+        if(_top<=0)
         {
             _stack.push_back(val);
+        }else
+        {
+            if(_stack[_top-1] < val)
+            {
+                _stack.push_back(_stack[_top-1]);
+            }else
+            {
+                _stack.push_back(val);
+            }
         }
-        top++;
+        _val_stack.push_back(val);
+        _top++;
     }
     
     void pop() {
-        
+        _stack.pop_back();
+        _val_stack.pop_back();
+        _top--;
     }
     
     int top() {
-        
+        return _val_stack[_top-1];
     }
     
     int getMin() {
-        
+        return _stack[_top-1];
     }
 };
 
