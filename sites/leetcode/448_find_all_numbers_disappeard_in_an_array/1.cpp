1ec619b1b1513079022c6048ce047ffee425bf74
noelbird <lduldu00228@gmail.com>
2021-11-18 23:59:36 +0900

    448

---
+++ sites/leetcode/448_find_all_numbers_disappeard_in_an_array/1.cpp
@@ -0,0 +1,19 @@
+#include<algorithm>
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> res;
+        
+        for(int i=0; i<nums.size(); ++i)
+        {
+            printf("%d ", nums[i]);
+            if(nums[i] != i+1)
+            {
+                res.push_back(i+1);
+            }
+        }
+        return res;
+    }
+};
