6d4dbc7bfe594c2d25142d0dd2dbd9feb5d14b68
noelbird <lduldu00228@gmail.com>
2021-12-29 00:15:37 +0900

    update leet code 127_first_unique_character_in_a_string

--- sites/leetcode/127_first_unique_character_in_a_string/1.cpp
+++ sites/leetcode/127_first_unique_character_in_a_string/1.cpp
@@ -1,24 +1,32 @@
+// runtime: 56ms
+// slow
+
 class Solution {
 public:
     int firstUniqChar(string s) {
         int idx;
+        bool impossibleArr[100001] = {0,};
+        bool isFound;
         for(int i=0; i<s.size(); ++i)
         {
-            int idx=i;
-            bool flag=false;
+            if(impossibleArr[i]==true) continue;
+            idx=i;
+            isFound=true;
             for(int j=i+1; j<s.size(); ++j)
             {
-                if(i==j)
+                if(s[i]==s[j])
                 {
-                    flag=true;
-                    break;
+                    isFound=false;
+                    impossibleArr[i] = true;
+                    impossibleArr[j] = true;
                 }
             }
-            if(!flag)
+            if(isFound)
             {
-                cout << idx << endl;
+                break;
             }
         }
+        if(!isFound) return -1;
+        return idx;
     }
-    return idx;
 };
