f188f88e52bd2082461da04332d992eb35ba4768
noelbird <lduldu00228@gmail.com>
2022-02-23 00:11:10 +0900

    update 2_Senate_Evacuation

--- sites/google_codejam/2018/2_Senate_Evacuation/1.py
+++ sites/google_codejam/2018/2_Senate_Evacuation/1.py
@@ -1,15 +1,31 @@
+# 논리적으로 헷갈렸음
+# 맨 마지막만 2명이 되면 그 때만 2명이 한꺼번에 빠지면 된다고 생각했는데,
+# 정당 2개인 경우라면 항상 같이 빠져야 했음
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     l = list(map(int, input().split()))
     result = []
     while True:
-        if sum(l) == 2:
-            idx1 = l.index(1)
-            l[idx1] -= 1
-            idx2 = l.index(1)
-            result.append(chr(idx1 + 0x41) + chr(idx2+0x41))
-            break
+        cnt = 0
+        for i in range(len(l)):
+            if l[i] != 0:
+                cnt += 1
+        if cnt == 2:
+            idx1 = -1
+            idx2 = -1
+            for i in range(len(l)):
+                if l[i] != 0:
+                    if idx1 < 0:
+                        idx1 = i
+                    else:
+                        idx2 = i
+                        break
+            if l[idx1] == l[idx2]:
+                result.append(chr(idx1 + 0x41) + chr(idx2+0x41))
+                l[idx1] -= 1
+                l[idx2] -= 1
+                continue
         if sum(l) == 0:
             break
         # find index of max value
